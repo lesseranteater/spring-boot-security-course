2c7bf917ae2e0c719827e09a69671db8edd053fa
alex <alex.iconia.tab@gmail.com>
2022-03-05 15:58:20 +0200

    Code formatting

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -13,9 +13,9 @@ import java.util.List;
 public class StudentController {
 
     private static final List<Student> STUDENTS = Arrays.asList(
-      new Student(1, "James Bond"),
-      new Student(2, "Maria Jones"),
-      new Student(3, "Anna Smith")
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Anna Smith")
     );
 
     @GetMapping(path = "{studentId}")
